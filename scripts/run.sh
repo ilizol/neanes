0a9f63978d75b4cad145e60f8cb269dd01816e37
Elias Zolotas <ilizol@yahoo.gr>
2025-06-21 10:08:10 +0300

    Update scripts folder.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,3 @@
 ../dist/neanes-cli --silent-pdf ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
+
+../dist/neanes-cli --silent-html ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
