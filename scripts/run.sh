c74a0042640eafb9ebf255b385bcb0f932324ff6
Elias Zolotas <ilizol@yahoo.gr>
2025-07-12 12:39:40 +0300

    Update scripts folder.

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,10 @@
 # corepack enable npm && corepack prepare --activate
 # npm install
 
+# kontakion_prophet_elias
 ../dist/neanes-cli --silent-pdf ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
 
 ../dist/neanes-cli --silent-html ~/Projects/neanes-examples/kontakion_prophet_elias/kontakion_prophet_elias_almouzios.byzx
+
+# tests
+find  ~/Projects/neanes-examples/tests -type f -name '*.byzx' -print0 | xargs -0 ../dist/neanes-cli --silent-pdf
